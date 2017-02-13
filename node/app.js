5dd38c63386fdddd9133c3495053c16626faba95
akkidg <akashg@zabuzalabs.com>
2017-02-13 10:23:52 +0530

    app.js update

--- node/app.js
+++ node/app.js
@@ -24,7 +24,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 
-
 /*
  * Be sure to setup your config values before running this code. You can 
  * set them using environment variables or modifying the config file in /config.
